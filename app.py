5da48f1807ff8ba09a419e045e1e79581aa760a9
lafnowcrylater <Ponake24565@gmail.com>
2025-03-14 04:47:31 +0700

    fix

--- app.py
+++ app.py
@@ -88,6 +88,6 @@ if uploaded_file is not None:
     else:
         predicted_class = "Real"  # or your class label for 0
 
-    st.header("### Prediction Results")
+    st.header("Prediction Results")
     st.subheader(f"**Predicted Class:** {predicted_class}")
     #st.write(f"**Confidence:** {predicted_prob:.2f}" if predicted_class == "Positive" else f"**Confidence:** {1 - predicted_prob:.2f}")
