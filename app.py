c9aa8cc86ab6c1bc74fd14cf41973f1b925542a9
lafnowcrylater <Ponake24565@gmail.com>
2025-03-14 04:12:29 +0700

    fix

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 if uploaded_file is not None:
     # Open the image file
     image = PIL.Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
     
     st.write("Processing image...")
 
@@ -79,7 +79,7 @@ if uploaded_file is not None:
     st.write(poor.shape)
     
     # Get prediction from the model
-    predictions = model.predict([rich, poor])
+    predictions = model.predict({rich, poor})
 
     # Decide the class based on a threshold (0.5 is a common choice)
     predicted_prob = predictions[0][0]  # assuming model output shape is (1, 1)
